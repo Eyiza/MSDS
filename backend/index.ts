5ce1435be6d35f1a1f715d88c0cd93a2ac40541c
Precious Michael <precious.michael2002@gmail.com>
2025-05-17 14:34:55 +0100

    add: mongodb config

--- backend/index.ts
+++ backend/index.ts
@@ -1,21 +1,31 @@
 import Fastify from 'fastify';
 import dotenv from 'dotenv';
+import mongoose from "mongoose";
 
 dotenv.config();
 
-const fastify = Fastify({ logger: true });
+const app = Fastify();
+const PORT = Number(process.env.PORT) || 5000;
+const MONGODB_URL = process.env.MONGODB_URL || "";
 
-fastify.get('/', async () => {
-  return { message: 'MSDS Backend Running' };
+
+// Routes
+app.get('/', async (req, res) => {
+  res.send("MSDS Backend Running");
 });
 
-// Start server
+// Start server and connect to MongoDB
 const start = async () => {
   try {
-    await fastify.listen({ port: Number(process.env.PORT) || 3000, host: '0.0.0.0' });
-    console.log(`Server running on http://localhost:${process.env.PORT || 3000}`);
+    console.log('Connecting to MongoDB...');
+    await mongoose.connect(MONGODB_URL);
+    console.log('Connected to MongoDB');
+
+    await app.listen({ port: PORT, host: '0.0.0.0' });
+    console.log(`Server running at http://localhost:${PORT}`);
   } catch (err) {
-    fastify.log.error(err);
+    console.error('MongoDB connection error:', err);
+    app.log.error(err);
     process.exit(1);
   }
 };

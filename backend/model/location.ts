afb72140383cd52f551a0cfb15637d0901a2d7c6
Precious Michael <precious.michael2002@gmail.com>
2025-06-13 02:21:57 +0100

    add: location schema

---
+++ backend/model/location.ts
@@ -0,0 +1,32 @@
+import { Schema, model, Document } from 'mongoose';
+
+// Define the Location interface extending Document for Mongoose
+export interface ILocation extends Document {
+  name: string;
+  coordinates: {
+    x: number;
+    y: number;
+  };
+  robot: Schema.Types.ObjectId;
+  description: string;
+  type: string;
+  createdAt: Date;
+}
+
+const LocationSchema = new Schema({
+  name: { type: String, required: true },
+  coordinates: {
+    x: Number,
+    y: Number,
+  },
+  robot: {
+    type: Schema.Types.ObjectId,
+    ref: 'Robot',
+    required: true,
+  },
+  description: { type: String, default: '' },
+  type: { type: String, enum: ['ward', 'base', 'room'], default: 'room' },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default model<ILocation>('Location', LocationSchema);
